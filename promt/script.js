3a5767efafb17b7c92a2e961ea9fd168956fda1d
AmmarAhmad1234 <154312036+AmmarAhmad1234@users.noreply.github.com>
2024-03-07 23:27:08 +0500

    Done

---
+++ promt/script.js
@@ -0,0 +1,9 @@
+let Firstname =prompt ("Enter Your First Name");
+
+let Lastname =prompt ("Enter Your Last Name");
+
+let Middlename =prompt ("Enter Your Middle Name");
+
+console.log(`${Firstname} ${Lastname} ${Middlename}`);
+
+
